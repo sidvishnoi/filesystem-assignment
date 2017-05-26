77b970595d21473407f12935eec71347a1428c32
sidvishnoi <sidvishnoi8@gmail.com>
2017-05-26 13:56:03 +0530

    Add day 2 base level tasks

--- day2/getStatus.cpp
+++ day2/getStatus.cpp
@@ -1,6 +1,43 @@
 #include "../filesystem.h"
 
 int FileSystem::getStatus(int sector_no) {
+    /*
+    objective: to get status of a given sector
+    input:
+        sector_no = sector number
+    return:
+        status of a sector
+    */
 
-    return 0;
+    if (sector_no > numberOfSectors_k - 1) {
+      	cout << "ERROR: Sector does not exist." << endl;
+		return NOT_FOUND;
+    }
+  	if (sector_no < reservedSectors_k) {
+      	cout << "ERROR: Sector is reserved." << endl;
+    	return RESERVED;
+    }
+
+  	// cast will convert a char[4] array to an int, using type-punning
+    TypeCastInt cast;
+
+    /* %%
+    Compute which to sector read from, as minimum unit of read/write is a sector
+    >> based on sector_no, computer a <sectorOffset> which should have the status for sector_no
+    >> also, compute a <byteOffset> which shows starting from which byte in <sectorOffset>, next 4 bytes need to be read
+    (hint: same as in setStatus())
+    */
+    int sectorOffset, byteOffset;
+  	// compute here
+
+    char buffer[sectorSize_k];
+    readSector(sectorOffset, buffer);
+
+    // edit buffer
+    for (int i = byteOffset, j = 0; j < 4; ++i, ++j) {
+        cast.str[j] = buffer[i];
+    }
+
+    return cast.num;
+
 }
--- day2/setStatus.cpp
+++ day2/setStatus.cpp
@@ -1,6 +1,48 @@
 #include "../filesystem.h"
 
 int FileSystem::setStatus(int sector_no, int x) {
+    /*
+    objective: to update status of a given sector
+    input:
+        sector_no = sector number
+        x = status (0 = FREE, 1 = BUSY, other: continuation sector number)
+    return:
+        (failure) -1
+        (success) 0
+    effect: a sector is marked in disk
+    */
+
+    if (sector_no > numberOfSectors_k - 1) {
+      	cout << "ERROR: Sector does not exist." << endl;
+		return -1;
+    }
+  	if (sector_no < reservedSectors_k) {
+      	cout << "ERROR: Sector is reserved." << endl;
+    	return -1;
+    }
+
+  	// cast will convert an int <x> to a char[4] array, using type-punning
+    TypeCastInt cast;
+    cast.num = x;
+
+    /* %%
+    Compute which to sector update, as minimum unit of read/write is a sector
+    >> based on sector_no, computer a <sectorOffset> which should have the status for sector_no
+    >> also, compute a <byteOffset> which shows starting from which byte in <sectorOffset>, next 4 bytes need to be updated
+    */
+    int sectorOffset, byteOffset;
+  	// compute here
+
+    char buffer[sectorSize_k];
+    readSector(sectorOffset, buffer);
+
+    // edit buffer
+    for (int i = byteOffset, j = 0; j < 4; ++i, ++j) {
+        buffer[i] = cast.str[j];
+    }
+
+    writeSector(sectorOffset, buffer);
 
     return 0;
+
 }
