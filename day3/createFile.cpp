c5250e8e120fd001ae09f76e9e202006560770b2
Sid Vishnoi <sidvishnoi8@gmail.com>
2017-05-27 21:33:54 +0530

    Format indentations and code-style

--- day3/createFile.cpp
+++ day3/createFile.cpp
@@ -6,43 +6,43 @@
 
 int FileSystem::createFile(const char *title, const char *srcFile) {
     /*
-     Objective: To create a file
-     Input parameters:
-     title:  file name
-     srcfile: file to read data from
-     Return value:
-     0: success
-     1: failure
-     Effect: a new file is created in filesystem and it's entry is passed in current working directory
+    Objective: To create a file
+    Input parameters:
+        title:  file name
+        srcfile: file to read data from
+    Return value:
+        0: success
+        1: failure
+    Effect: a new file is created in filesystem and it's entry is passed in current working directory
      */
-    
+
     TypeCastEntry entry;
     if (strlen(title) == 0) {
         // %% get file name from user and store it in entry.entry.name
     } else {
         strcpy(entry.entry.name, title);
     }
-    
+
     /* %% use alreadyExists() to check for duplicate entry) */
     bool fileAlreadyExists; // %%
-    
+
     if(fileAlreadyExists) {
         cout << "ERROR: File <" << entry.entry.name<< "> already exists!" << endl;
         return 1;
     }
-    
+
     vector<int>offset = getEntryPosition();
-    
+
     /* %% use offset to check if space for file entry is available */
     bool enoughSpaceForEntry;
     if(enoughSpaceForEntry) {
         cout << "ERROR: Not enough space for file entry." << endl;
         return 1;
     }
-    
+
     // file contents & size
     char file_content[MAX_FILE_SIZE];
-    
+
     if (strlen(srcFile) == 0) {
         // if source file is not given
         // %% get data from user into file_content and calculate size in bytes to entry.entry.size
@@ -52,26 +52,26 @@ int FileSystem::createFile(const char *title, const char *srcFile) {
         // %% read the content from source file into file_content and calculate size (bytes)
         fsrc.close();
     }
-    
+
     if (entry.entry.size < 0) return 1;
-    
+
     // find sectors to write in
     int sectorsNeeded = (entry.entry.size - 1) / sectorSize_k + 1;
     vector<int> sectorsFree = findEmptySectors(sectorsNeeded);
-    
+
     // %% check if vector returned is empty
     bool enoughSectors;
     if (!enoughSectors) {
         cout << "ERROR: Not enough memory!" << endl;
         return 1;
     }
-    
+
     // %% fill details of file entry (parent, file type)
     // parent is sector corresponding to (current) root directory
     entry.entry.startsAt = sectorsFree[0];
-    
+
     writeEntry(offset, entry.str);
-    
+
     char buf[sectorSize_k];
     // write data and update sector status
     for (int i = 0; i < sectorsNeeded; ++i) {
--- day3/readFile.cpp
+++ day3/readFile.cpp
@@ -2,39 +2,37 @@
 
 int FileSystem::readFile(const char *title) {
     /*
-     Objective: To a read a file
-     Inpute parameters:
-     title: File name
-     Return type:	int
-     0: success
-     1: error
-     */
+    Objective: To a read a file
+    Input parameters:
+        title: File name
+    Return:
+        0: success
+        1: error
+    Output: contents of file
+    */
     TypeCastEntry entry;
     if (strlen(title) == 0) {
         // %% get file name from user in entry.entry.name
     } else {
         strcpy(entry.entry.name, title);
     }
-    
-    vector<int>position;
-    position=alreadyExists(entry.entry.name,'f');
+
+    vector<int>position = alreadyExists(entry.entry.name, 'f');
     // %% use position to check if given file exists
     bool exists;
-    if(!exists)
-    {
-        // %% dsplay error message
+    if(!exists) {
+        // %% display error message
         return 1;
     }
-    
+
     char buffer[sectorSize_k];
     // %% read sector offset returned by alreadyExists() in "buffer"
-    
+
     //reading file entry in entry.str
     for( int i = 0 ; i < 32 ; ++i) entry.str[i] = buffer[position[1] + i];
-    
+
     int sector = entry.entry.startsAt;
-    while( sector!= 1 )
-    {
+    while (sector != 1) {
         // %% Display content of sector
         sector = getStatus(sector);
     }
